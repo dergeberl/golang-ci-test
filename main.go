e876d390e953b0cfca9d493ea6f784bf2810ceba
Maximilian Geberl <48486938+dergeberl@users.noreply.github.com>
2021-02-04 21:16:50 +0100

    add errors in main.go for testing

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Hello World")
+	HelloWorld()
+	// test go vet
+	var world string
+	go func() {
+		world = "hello"
+	}()
+	//gosec test
+	username := "admin"
+	password := "CQP7qcDT6DZqh2qoqpvTTz8"
+	_ = fmt.Sprintf("%v %v", username, password)
+}
+
+func HelloWorld(){
+	fmt.Println("Hello World from func")
 }
