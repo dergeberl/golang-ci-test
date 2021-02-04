c891b75994533ac23865e0e758b2bb89c523a35b
Maximilian Geberl <48486938+dergeberl@users.noreply.github.com>
2021-02-04 21:24:47 +0100

    fix gosec

--- main.go
+++ main.go
@@ -14,9 +14,9 @@ func main() {
 	//}()
 
 	//gosec test
-	username := "admin"
-	password := "CQP7qcDT6DZqh2qoqpvTTz8"
-	_ = fmt.Sprintf("%v %v", username, password)
+	//username := "admin"
+	//password := "CQP7qcDT6DZqh2qoqpvTTz8"
+	//_ = fmt.Sprintf("%v %v", username, password)
 }
 
 //HelloWorld returns Hello World
