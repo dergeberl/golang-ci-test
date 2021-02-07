6e9aade3271f727c9f1c384bc52b59708f52a3ad
Maximilian Geberl <48486938+dergeberl@users.noreply.github.com>
2021-02-07 14:53:48 +0100

    add test

--- main_test.go
+++ main_test.go
@@ -2,9 +2,16 @@ package main
 
 import "testing"
 
-func TestSolveDay1Part1(t *testing.T) {
+func TestSolveFakeTest1(t *testing.T) {
 	t.Run("fake test", func(t *testing.T) {
 		HelloWorld()
+		//t.Errorf("fail")
+	})
+}
+
+func TestSolveFakeTest2(t *testing.T) {
+	t.Run("fake test", func(t *testing.T) {
+		main()
 		//t.Errorf("fail")
 	})
 }
